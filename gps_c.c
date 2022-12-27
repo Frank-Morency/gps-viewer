b5def531f454d89b5545d79ef57dec7469513dc0
Francis Morency <frank12356@gmail.com>
2022-12-27 16:22:16 -0500

    started adding selection output

--- gps_c.c
+++ gps_c.c
@@ -3,7 +3,9 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <termios.h>
-
+#include <string.h>
+/*need to add command line argument to select whats printed on screen
+? print form with result displayed with an update frequency*/
 struct termios term;
 
 int openFd();
@@ -13,34 +15,47 @@ int main(void)
 {
 	int fd;
 	char buf[100];
-	int i;
-	i = 0;
+	int i = 0;
+
 	fd = openFd();	
 	setPort(fd);
+
 	while (fd)
 	{
 		i = 0;
 
 		read(fd, &buf, 100);
+		char ref[7] = "$GPGGA";
+
 		if (*buf == '$')
 		{	
-			printf("%s\n", &*buf);
+			char out[100];
+			strcpy(out, buf);
+			if(memcmp(buf, ref, 6) == 0)
+			{
+				printf("%s\n",out);
+			}
+			//printf("*****%s\n",out);
+			//printf("%s\n", &*buf);
 			while (i < 100)
 				buf[i++] = '\0';
 		}
 	}
 	close(fd);
+
 	return(0);
 }
 
 int openFd(){
 	int fd;
 	fd = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY | O_SYNC);
+
 	if (fd == -1)
 	{
 		printf("/dev/ttyUSB0 unavailable. %d\n", fd);
 		exit (0);
 	}
+
 	return (fd);
 }
 
