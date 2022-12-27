aba48a08be827eea63ef6dac7d93068834cc4f51
Frank-Morency <79029998+Frank-Morency@users.noreply.github.com>
2022-12-26 19:39:38 -0500

    Add files via upload
    add gps_c to a new public repo with licence

---
+++ gps_c.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <termios.h>
+
+struct termios term;
+
+int main(void)
+{
+	int fd;
+	char buf[100];
+	int i;
+	
+	i = 0;	
+	fd = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY | O_SYNC);
+	if (fd == -1)
+	{
+		printf("/dev/ttyUSB0 unavailable. %d\n", fd);
+		exit (0);
+	}
+	printf("%d test", fd);
+	tcgetattr(fd, &term);
+	cfsetspeed(&term, B4800);
+	tcsetattr(fd, TCSANOW, &term);
+
+	while (fd)
+	{
+		i = 0;
+
+		read(fd, &buf, 100);
+		if (*buf == '$')
+		{	
+			printf("%s\n", &*buf);
+			while (i < 100)
+				buf[i++] = '\0';
+		}
+	}
+	close(fd);
+	return(1);
+}
+//stty - F /dev/ttyUSB0 4800
