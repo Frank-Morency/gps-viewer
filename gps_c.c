ded5bff118babd9981393132f6f1921d03b24f8d
forency <fmorency@student.42quebec.com>
2022-12-29 16:27:33 -0500

    some change

--- gps_c.c
+++ gps_c.c
@@ -4,33 +4,29 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <string.h>
-/*If you run the program without any argument, every sentence will show on screen
-if you want to print only one type of sentence run it as in exemple: "name of prog" \$GPGGA
-this will only show $GPGGA sentence on screen
+/*if you want to pipe 
+only one type of sentence run it as in:
+exemple: "name of prog" \$GPGGA
+this will only take $GPGGA sentence on pipe*/
 
-? print form with result displayed with an update frequency*/
-struct termios term;
+int		openFd(void);
+void	setPort(int fd);
 
-int openFd();
-void setPort(int fd);
-
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
+	char	ref[100] = "";
+	int		input = 0;
+	int		fd;
+	char	buf[100];
+	char	out[100];
+	char	gpgga[] = "$GPGGA";
+	char	gpgsa[] = "$GPGSA";
+	char	gpgsv[] = "$GPGSV";
+	char	gprmc[] = "$GPRMC";
+	char	test[] = "ALL";
 
-	char ref[100] = "";
-	int input = 0;
-	int fd;
-	char buf[100];
-	char gpgga[] = "$GPGGA";
-	char gpgsa[] = "$GPGSA";
-	char gpgsv[] = "$GPGSV";
-	char gprmc[] = "$GPRMC";
-	char test[] = "ALL";
-
-
-	fd = openFd();
-	setPort(fd);
-
+	fd = open_fd();
+	set_port(fd);
 	if (argc == 2)
 	{
 		strcpy(ref, argv[1]);
@@ -40,37 +36,44 @@ int main(int argc, char *argv[])
 		printf("type 1 for $GGPGA\ntype 2 for $GPGSA\ntype 3 for $GPGSV\ntype 4 for $GPRMC\n");
 		printf("Select which data sentence you want to see:(0 to display everything.)\n");
 		scanf(" %d", &input);
-
-		switch (input){
-			case (1):
+		switch (input)
+		{
+			case (1) :
+			{
 				strcpy(ref, gpgga);
-				break;
-			case (2):
+				break ;
+			}
+			case (2) :
+			{
 				strcpy(ref, gpgsa);
-				break;
-			case (3):
+				break ;
+			}
+			case (3) :
+			{
 				strcpy(ref, gpgsv);
-				break;
-			case (4):
+				break ;
+			}
+			case (4) :
+			{
 				strcpy(ref, gprmc);
-				break;
-			default:
-				//strcpy(ref, test);
-				break;
+				break ;
+			}
+			default :
+				break ;
 		}
 	}
-
 	while (fd)
 	{
 		read(fd, &buf, 100);
 		if (*buf == '$')
-		{
-			char out[100];
+		{	
 			strcpy(out, buf);
-			if (ref[0] != '$'){
+			if (ref[0] != '$')
+			{
 				printf("%s", out);
 			}	
-			else{
+			else
+			{
 				if (memcmp(buf, ref, 6) == 0)
 				{
 					printf("%s", out);
@@ -80,26 +83,26 @@ int main(int argc, char *argv[])
 		}
 	}	
 	close(fd);
-
 	return (0);
 }
 
-int openFd()
+int	open_fd(void)
 {
-	int fd;
-	fd = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY | O_SYNC);
+	int	fd;
 
+	fd = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY | O_SYNC);
 	if (fd == -1)
 	{
 		printf("/dev/ttyUSB0 unavailable. %d\n", fd);
 		exit(0);
 	}
-
 	return (fd);
 }
 
-void setPort(int fd)
+void	set_port(int fd)
 {
+	struct termios	term;
+
 	tcgetattr(fd, &term);
 	cfsetspeed(&term, B4800);
 	tcsetattr(fd, TCSANOW, &term);
