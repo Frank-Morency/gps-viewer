7e93efae1779c56073d18da38541582776c1a454
Francis Morency <frank12356@dell-server.lan>
2022-12-29 11:47:47 -0500

    added selection within prog

--- gps_c.c
+++ gps_c.c
@@ -14,57 +14,89 @@ struct termios term;
 int openFd();
 void setPort(int fd);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	char ref[100] = "$";
+	char ref[100] = "";
+	int input = 0;
 	int fd;
 	char buf[100];
-	
-	fd = openFd();	
+	char gpgga[] = "$GPGGA";
+	char gpgsa[] = "$GPGSA";
+	char gpgsv[] = "$GPGSV";
+	char gprmc[] = "$GPRMC";
+
+
+	fd = openFd();
 	setPort(fd);
+
+	if (argc == 2)
+	{
+		strcpy(ref, argv[1]);
+	}
+	else
+	{
+		printf("type 1 for $GGPGA\ntype 2 for $GPGSA\ntype 3 for $GPGSV\ntype 4 for $GPRMC\n");
+		printf("Select which data sentence you want to see:\n");
+		scanf("%d", &input);
+
+		switch (input){
+			case (1):
+				strcpy(ref, gpgga);
+				break;
+			case (2):
+				strcpy(ref, gpgsa);
+				break;
+			case (3):
+				strcpy(ref, gpgsv);
+				break;
+			case (4):
+				strcpy(ref, gprmc);
+				break;
+			default:
+				printf("no selection, every sentence will be shown");
+				break;
+		}
+	}
 
 	while (fd)
 	{
 		read(fd, &buf, 100);
 		if (*buf == '$')
-		{	
+		{
 			char out[100];
-			if (argc == 2) {
-				strcpy(ref, argv[1]);
-			}
-			else {
-				strcpy(ref, buf);
-			}
-
+			if (ref == NULL)
+				strcpy(ref, buf);	
 			strcpy(out, buf);
-			if(memcmp(buf, ref, 6) == 0)
+			if (memcmp(buf, ref, 6) == 0)
 			{
-				printf("%s\n",out);
+				printf("%s\n", out);
 			}
 			bzero(buf, 100);
 		}
 	}
 	close(fd);
 
-	return(0);
+	return (0);
 }
 
-int openFd(){
+int openFd()
+{
 	int fd;
 	fd = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY | O_SYNC);
 
 	if (fd == -1)
 	{
 		printf("/dev/ttyUSB0 unavailable. %d\n", fd);
-		exit (0);
+		exit(0);
 	}
 
 	return (fd);
 }
 
-void setPort(int fd){
+void setPort(int fd)
+{
 	tcgetattr(fd, &term);
 	cfsetspeed(&term, B4800);
 	tcsetattr(fd, TCSANOW, &term);
-
 }
