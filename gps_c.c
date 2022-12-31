de25d2a73294e7f773f29497ad49bedea3c3e542
Francis Morency <frank12356@dell-server.lan>
2022-12-30 23:33:10 -0500

    not to my liking yet

--- gps_c.c
+++ gps_c.c
@@ -15,26 +15,26 @@ If you dont put any argument you have to select at runtime*/
 #define PORT_SPEED B4800
 #define PORT "/dev/ttyUSB0"
 
-int		open_fd(void);
-void	set_port(int fd);
-void	menu(char *Pref);
-void	parse(char *out, char *Pref);
-float	kn_km(float knot);
-float	kn_mph(float knot);
-int	utc_loc(int utc);
-char	heading(float deg);
-float	m_f(float m);
-static int	w_cnt(char const *s, char c);
-int	c_cnt(char const *s, char c);
+int open_fd(void);
+void set_port(int fd);
+void menu(char *Pref);
+void parse(char *out, char *Pref);
+float kn_km(float knot);
+float kn_mph(float knot);
+int utc_loc(int utc);
+char heading(float deg);
+float m_f(float m);
+static int w_cnt(char const *s, char c);
+int c_cnt(char const *s, char c);
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	char	ref[100] = "x";
-	char	*Pref = ref;
-	int		fd;
-	char	buf[100];
-	char	out[100];
-	
+	char ref[100] = "x";
+	char *Pref = ref;
+	int fd;
+	char buf[100];
+	char out[100];
+
 	fd = open_fd();
 	set_port(fd);
 
@@ -46,13 +46,13 @@ int	main(int argc, char *argv[])
 	{
 		read(fd, &buf, 100);
 		if (*buf == '$')
-		{	
+		{
 			strcpy(out, buf);
 			if (ref[0] != '$')
 			{
 				printf("%s", out);
-//				parse(out, Pref);
-//				exit(0);
+				//parse(out, Pref);
+				//				exit(0);
 			}
 			else
 			{
@@ -61,129 +61,135 @@ int	main(int argc, char *argv[])
 			}
 			bzero(buf, 100);
 		}
-	}	
+	}
 	close(fd);
 	return (0);
 }
 
-int	open_fd(void)
+int open_fd(void)
 {
-	int	fd;
+	int fd;
 
 	fd = open(PORT, O_RDONLY | O_NOCTTY | O_SYNC);
 	if (fd == -1)
 	{
-		printf("%s unavailable. %d\n",PORT , fd);
+		printf("%s unavailable. %d\n", PORT, fd);
 		exit(0);
 	}
 	return (fd);
 }
 
-void	set_port(int fd)
+void set_port(int fd)
 {
-	struct termios	term;
+	struct termios term;
 
 	tcgetattr(fd, &term);
 	cfsetspeed(&term, PORT_SPEED);
 	tcsetattr(fd, TCSANOW, &term);
 }
 
-void menu(char *Pref){
-	int		input = 0;
-	char	gpgga[] = "$GPGGA";
-	char	gpgsa[] = "$GPGSA";
-	char	gpgsv[] = "$GPGSV";
-	char	gprmc[] = "$GPRMC";
-	char	test[] = "ALL";
+void menu(char *Pref)
+{
+	int input = 0;
+	char gpgga[] = "$GPGGA";
+	char gpgsa[] = "$GPGSA";
+	char gpgsv[] = "$GPGSV";
+	char gprmc[] = "$GPRMC";
+	char test[] = "ALL";
 
 	printf(" 1 for $GGPGA\n 2 for $GPGSA\n 3 for $GPGSV\n 4 for $GPRMC\n");
 	printf(" Select sentence you want :(0 to display all.)\n");
 	scanf(" %d", &input);
 	switch (input)
 	{
-		case (1) :
-			strcpy(Pref, gpgga);
-			break ;
-		case (2) :
-			strcpy(Pref, gpgsa);
-			break ;
-		case (3) :
-			strcpy(Pref, gpgsv);
-			break ;
-		case (4) :
-			strcpy(Pref, gprmc);
-			break ;
-		default :
-			strcpy(Pref, test); //not needed
-			break ;
-		}
+	case (1):
+		strcpy(Pref, gpgga);
+		break;
+	case (2):
+		strcpy(Pref, gpgsa);
+		break;
+	case (3):
+		strcpy(Pref, gpgsv);
+		break;
+	case (4):
+		strcpy(Pref, gprmc);
+		break;
+	default:
+		strcpy(Pref, test); // not needed
+		break;
+	}
 }
 
-void	parse(char *out, char *Pref)
-{	// need to parse needed information (which sentences) else all
-	int	i = 0;
-	int	j = 0;
-	int	k = 0;
+void parse(char *out, char *Pref)
+{ // need to parse needed information (which sentences) else all
+	int i = 0;
+	int j = 0;
+	int k = 0;
 	char skip = ' ';
 	char **parsed;
 	printf("parse\n");
 	parsed = (char **)malloc(sizeof(char *) * w_cnt(out, ',') + 1);
-	if (!parsed){
+	if (!parsed)
+	{
 		printf("Memory not allocated.\n");
 		exit(0);
 	}
 	while (out[i] != '\0' && out[i] != '\n' && i <= 100)
 	{
-		
-		while(out[i] != ',' && out[i] != '\n' && out[i] != '\0')
+
+		while (out[i] != ',' && out[i] != '\n' && out[i] != '\0')
 		{
 			parsed[j] = (char *)malloc(sizeof(char) * c_cnt(&out[i], ',') + 1);
-			if (!parsed[j]){
+			if (!parsed[j])
+			{
 				printf("oups!");
 				exit(0);
 			}
-			strncpy(parsed[j], &out[i], (c_cnt(out + i, ',') + 1));
-			//printf("%s", *parsed);
+			strncpy(parsed[j], &out[i], (c_cnt(out + i, ',')));
 			i++;
 			j++;
 		}
+		//Getting closer with following if//
+		if (j == 0)
+			*parsed[j] = '\0';
+		//printf("i=%d, j=%d, k=%d\t",i, j, k);
+		printf("%s\n", &**parsed);
+		printf("OUT %s\n", *parsed);
 		k++;
 		i++;
-		
 		j = 0;
-		printf("%d\t", k);
-		printf("OUT %s\n", *parsed);
+		//printf("%d\t", k);
+		//printf("OUT %s\n", *parsed);
 	}
-	//k = 0;
-	//put *out in an array splitted by coma?
+	// put *out in an array splitted by coma?
 	//?struct array 0= char 1= int 2= int 3= float 4= char etc...?
-	printf("%s-%s\n", parsed[0], *parsed);
+	//printf("%s-%s\n", parsed[0], *parsed);
 	free(parsed);
 }
 
-float	kn_km(float knot)
+float kn_km(float knot)
 {
 	return (knot * 1.852);
 }
 
-float	kn_mph(float knot)
+float kn_mph(float knot)
 {
 	return (knot * 1.150779);
 }
 
-int		utc_loc(int utc)
+int utc_loc(int utc)
 {
 	int local;
 	local = -5;
 	return (utc + local);
 }
 
-float	m_f(float m)
+float m_f(float m)
 {
 	return (m * 3.28084);
 }
 
-char	heading(float deg)
+char heading(float deg)
 {
 	char *dir[] = {"n", "ne", "e", "se", "s", "sw", "w", "nw"};
 	char direction;
@@ -207,10 +213,10 @@ char	heading(float deg)
 	return (direction);
 }
 
-static int	w_cnt(char const *s, char c)
+static int w_cnt(char const *s, char c)
 {
-	int	word;
-	int	i;
+	int word;
+	int i;
 
 	i = 0;
 	word = 0;
@@ -222,7 +228,7 @@ static int	w_cnt(char const *s, char c)
 			while (s[i] != c && s[i] != '\0')
 				i++;
 			if (s[i] == c)
-				continue ;
+				continue;
 		}
 		else
 			i++;
@@ -230,9 +236,9 @@ static int	w_cnt(char const *s, char c)
 	return (word);
 }
 
-int	c_cnt(char const *s, char c)
+int c_cnt(char const *s, char c)
 {
-	int	cnt;
+	int cnt;
 
 	cnt = 0;
 	while (*s != c && *s != '\0')
